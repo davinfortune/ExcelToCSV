66fa72bc597168e79c82990c7303603559b0f4b0
Davin Fortune <DFortune@kentstainless.com>
2020-11-17 14:58:33 +0000

    Added Exit Application Button

--- csv_converter.py
+++ csv_converter.py
@@ -12,17 +12,23 @@ def importExcel():
 def exportCSV():
     print("hello")
 
+def exitButton():
+    root.destroy()
+
 appHeading = Label(root, text = "Kent Excel to CSV Converter", font=('Helvetica 18 bold',15))
 importButton = Button(root, text = "Import File", font=('Helvetica 18 bold',25), command=importExcel,bg="green",fg="white",padx=20,pady=5)
-exportButton = Button(root, text = "Export CSV", font=('Helvetica 18 bold',23), command=exportCSV, bg="red",fg="white",padx=20,pady=5)
+exportButton = Button(root, text = "Export CSV", font=('Helvetica 18 bold',23), command=exportCSV, bg="blue",fg="white",padx=20,pady=5)
+exitAppButton = Button(root, text = "Exit Application", font=('Helvetica 18 bold',19), command=exitButton, bg="red",fg="white",padx=20,pady=5)
 
 appHeading.grid(row =0, column=0)
 importButton.grid(row=1, column=0)
-exportButton.grid(row=1, column=0)
+exportButton.grid(row=2, column=0)
+exitAppButton.grid(row=3, column=0)
 
 appHeading.place(relx=0.5, rely=0.1, anchor=CENTER)
 importButton.place(relx=0.5, rely=0.3, anchor=CENTER)
 exportButton.place(relx=0.5, rely=0.5, anchor=CENTER)
+exitAppButton.place(relx=0.5, rely=0.7, anchor=CENTER)
 
 
 
